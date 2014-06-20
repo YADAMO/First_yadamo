632b93cfdad15d884a3c5ccfaccac39467c35fc3
yutakikuya <b1010234@fun.ac.jp>
2014-06-20 18:35:59 +0900

    modify constractor, destractor and changeSection. add getCurSection.

--- SectionController.cpp
+++ SectionController.cpp
@@ -2,12 +2,20 @@
 
 SectionController::SectionController()
 {
+	cur = FIRST_SECTION;
 }
 
 SectionController::~SectionController()
 {
 }
 
-void SectionController::changeSection()
+void SectionController::changeSection(int setting_position)
 {
+	cur = setting_position;
+}
+
+//必要であると感じました
+int SectionController::getCurPosition()
+{
+	return cur;
 }
--- SectionController.h
+++ SectionController.h
@@ -3,12 +3,15 @@
 //include header file of ecorrent position estimate
 #include "LineTracer.h"
 
+enum Sections {FIRST_SECTION, SCOND_SECTION, };
+
 class SectionController
 {
 public:
 	SectionController();
 	~SectionController();
-	void changeSection();
+	void changeSection(int setting_position);
+	int getCurPosition();
 private:
-
+	int cur;
 };
