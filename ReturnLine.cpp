6533a900173c6079eb11dbf22e9a1c2585fcda4a
katoutakefumi <g2114006@fun.ac.jp>
2014-08-30 03:04:59 +0900

    returnlineいじりました

--- ReturnLine.cpp
+++ ReturnLine.cpp
@@ -1,10 +1,11 @@
 #include "ReturnLine.h"
 
-ReturnLine::ReturnLine(Driver *argDriver, LightSensor *argLightSensor)
+ReturnLine::ReturnLine(Driver *argDriver, LightSensor *argLightSensor, ColorDetector *argColorDetector)
 {
 	returnLine_time = 0;
 	driver = argDriver;
   	lightSensor = argLightSensor;
+  	ColorDetector = argColorDetector;
 }
 
 ReturnLine::~ReturnLine()
@@ -14,17 +15,21 @@ ReturnLine::~ReturnLine()
 void ReturnLine::returnLine()
 {
 	if(returnLine_time < 3000){
-		driver->drive(-40, 40);
+		driver->drive(-40, 20);
 	}else if(returnLine_time < 5000){
 		driver->drive(-40, 0);
 	}else if(returnLine_time < 8000){
-		driver->drive(-40, -40);
+		driver->drive(-40, -20);
 	}else if(returnLine_time < 11000){
 		driver->drive(0, 0);
 	}else if(returnLine_time < 13000){
-		driver->drive(40, 0);
+		driver->drive(40, 20);
 	}else if(returnLine_time < 16000){
-		driver->drive(0, 0);	
+		driver->drive(40, 0);
+	}else if(returnLine_time < 18000){
+		driver->drive(40, -20);
+	}else{
+		driver->drive(0, 0);
 	}
 	returnLine_time += 4;
 }
