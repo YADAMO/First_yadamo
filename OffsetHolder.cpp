e8ae54a54f32397b7070a2309d3e99ddc436099d
yutakikuya <g2114009@fun.ac.jp>
2014-09-30 18:59:16 +0900

    dark place and bright place calibration

--- OffsetHolder.cpp
+++ OffsetHolder.cpp
@@ -6,18 +6,30 @@ OffsetHolder::OffsetHolder(){
 OffsetHolder::~OffsetHolder(){
 }
 
-void OffsetHolder::setBlack(S16 value){
-	black = value;
+void OffsetHolder::setBlack(S16 value, bool place){
+	if     (place == BRIGHT_PLACE) black_bright = value;
+	else if(place == DARK_PLACE) black_dark = value;
+}
+
+S16 OffsetHolder::getBlack(bool place){
+	if     (place == BRIGHT_PLACE) return black_bright;
+	else if(place == DARK_PLACE) return black_dark;
 }
 
 S16 OffsetHolder::getBlack(){
-	return black;
+	return black_bright;
 }
 
-void OffsetHolder::setWhite(S16 value){
-	white = value;
+void OffsetHolder::setWhite(S16 value,bool place){
+	if     (place == BRIGHT_PLACE) white_bright = value;
+	else if(place == DARK_PLACE) white_dark = value;
+}
+
+S16 OffsetHolder::getWhite(bool place){
+	if     (place == BRIGHT_PLACE) return white_bright;
+	else if(place == DARK_PLACE) return white_dark;
 }
 
 S16 OffsetHolder::getWhite(){
-	return white;
+	return white_bright;
 }
